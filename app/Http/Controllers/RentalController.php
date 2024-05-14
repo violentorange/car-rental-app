b63b67f937eb6e61327f6ffc79e6dddd72ba24f5
lorincz.gergely.tamas <lorincz.gergely.tamas@students.jedlik.eu>
2024-05-14 17:37:35 +0200

    Authentication system done

--- app/Http/Controllers/RentalController.php
+++ app/Http/Controllers/RentalController.php
@@ -6,6 +6,8 @@ use App\Http\Resources\RentalResource;
 use App\Models\Car;
 use App\Models\Rental;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class RentalController extends Controller
 {
@@ -14,7 +16,20 @@ class RentalController extends Controller
      */
     public function index(Car $car)
     {
-        return RentalResource::collection($car->rentals);
+        $rentals = $car->rentals;
+        $user = Auth::user();
+
+        $rentals = $rentals->filter(function ($rental) use ($user) {
+            return $rental->user_id === $user->id;
+        });
+        return RentalResource::collection($rentals);
+
+        // $rentals = DB::table('rentals')
+        //     ->where('car_id', $car->id)
+        //     ->where('user_id', Auth::user()->id)
+        //     ->get();
+
+        // return ($rentals);
     }
 
     /**
@@ -26,6 +41,7 @@ class RentalController extends Controller
             'car_id' => $car->id,
             'start_date' => $request->start_date,
             'end_date' => $request->end_date,
+            'user_id' => $request->user()->id,
         ]);
         return new RentalResource($rental);
     }
@@ -33,24 +49,47 @@ class RentalController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Car $car, Rental $rental )
+    public function show( Rental $rental )
     {
+
+        if ($rental->user_id !== Auth::user()->id) {
+            return response()->json([
+               'message' => 'You are not authorized to perform this action',
+            ], 403);
+        }
+
         return new RentalResource($rental);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Car $car, Rental $rental)
+    public function update(Request $request, Rental $rental)
     {
-        //
+
+        if ($rental->user_id !== Auth::user()->id) {
+            return response()->json([
+               'message' => 'You are not authorized to perform this action',
+            ], 403);
+        }
+
+        $rental->update($request->all());
+        return new RentalResource($rental);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Car $car, Rental $rental)
+    public function destroy( Rental $rental)
     {
-        //
+
+        if ($rental->user_id !== Auth::user()->id) {
+            return response()->json([
+               'message' => 'You are not authorized to perform this action',
+            ], 403);
+        }
+
+        $rental->delete();
+        return response()->noContent();
     }
 }
